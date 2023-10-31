3d6ffaf0f0efe21d51fed6a43e9dd15ed998d18d
Maximilian Klassen <extern.mklassen@novomind.com>
2023-10-31 14:30:38 +0100

    wip

--- service.js
+++ service.js
@@ -1,6 +1,36 @@
 chrome.commands.onCommand.addListener(command => {
-    debug(command);
+    switch (command) {
+        case "moveLeft":
+            moveLeft();
+            break;
+        case "moveRight":
+            moveRight();
+            break;
+        case "jumpLeft":
+            jumpLeft();
+            break;
+        case "jumpRight":
+            jumpRight();
+            break;
+    }
 });
+
+async function getAllTabs() {
+    let windowId = (await chrome.windows.getCurrent()).id;
+    return await chrome.tabs.query({
+        windowId: windowId
+    });
+}
+
+async function getSelectedTabs() {
+    return await chrome.tabs.query({
+        highlighted: true
+    });
+}
+
+function move(tabId, index) {
+    chrome.tabs.move(tabId, { index: index });
+}
 
 async function debug(command) {
     let selectedTabs = await chrome.tabs.query({
@@ -10,4 +40,39 @@ async function debug(command) {
     chrome.tabs.move(selectedTabs[0].id, {
         index: index + 1
     });
+}
+
+async function moveLeft() {
+    let tabs = (await getSelectedTabs());
+    let shifted = false;
+
+    for (let i = 0, tab = tabs[i]; i < tabs.length; i++, tab = tabs[i]) {
+        if (!shifted) {
+            let index = tab.index;
+            let newIndex = await getPreviousSlot(index);
+            move(tab.id, newIndex);
+            if (newIndex > index) {
+                shifted = true;
+            }
+        }
+    }
+}
+
+async function moveRight() {
+
+}
+
+async function jumpLeft() {
+
+}
+
+async function jumpRight() {
+
+}
+
+async function getPreviousSlot(index) {
+    if (index - 1 < 0) {
+        return (await getAllTabs()).length - 1;
+    }
+    return index -1;
 }
